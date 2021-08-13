5ea832fc35fc7e3ee22165c4147e941c28a63807
Truong Duong Khang <truongduongkhang277@gmail.com>
2021-08-14 01:59:19 +0700

    commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,19 +4,184 @@ import data from '../data'
 
 export default function HomeScreen() {
     return (
-        <div className="small-container">
-            <h2 class="title">Feadtured Products</h2>
-            
-            <div className="row">
-                {
-                    data.products.map(
-                        product => (
-                            <Product key={product._id} product = { product }></Product>
+        <div>
+            <div className="header">        
+                <div className="container">
+                    <div className="navbar">
+                        <div className="logo">
+                            <a href="/"><img src="../images/logo.png" alt="Logo Store" width="250px"></img></a>
+                        </div>
+                        <nav>
+                            <ul id = "MenuItems">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/product">Product</a></li>
+                                <li><a href="/about">About</a></li>
+                                <li><a href="/contact">Contact</a></li>
+                                <li><a href="/signin">Account</a></li>
+                            </ul>
+                        </nav>
+                        <a href="/cart"><img src="../images/cart.png" alt="icon cart" className="cart-icon"></img></a>
+                        <img src="../images/menu.png" alt="icon menu" className="menu-icon"></img>
+                    </div>
+                    <div className="row">
+                        <div className="col-2">
+                            <h1>Give Your Phone<br /> A New Style !</h1>
+                            <p>Success isn't always about greatness. It's about consistency. Consistent<br /> hard work gain success. Greatness will come.</p>
+                            <a href="/" className="btn">Explore Now &#8594;</a>
+                        </div>
+                        <div className="col-2">
+                            <img src="../images/image1.png" alt="banner"></img>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+
+            <div className="category">
+                <div className="small-container">
+                    <h2 class="title">Cateogry</h2>
+                    <div className="row">
+                        <div className="col-3">
+                            <img src="../images/category-1.jpg" alt="category 1 image"></img>
+                        </div>
+                        <div className="col-3">
+                            <img src="../images/category-2.jpg" alt="category 2 image"></img>
+                        </div>
+                        <div className="col-3">
+                            <img src="../images/category-3.jpg" alt="category 3 image"></img>
+                        </div>
+                    </div>            
+                </div>
+            </div>
+
+            <div className="small-container">
+                <h2 class="title">Feadtured Products</h2>
+                
+                <div className="row">
+                    {
+                        data.products.map(
+                            product => (
+                                <Product key={product._id} product = { product }></Product>
+                            )
                         )
-                    )
-                }
+                    }
+                </div>
+            </div>
+
+            <div className="offer">
+                <div className="small-container">
+                    <div className="row">
+                        <div className="col-2">
+                            <img src="../images/exclusive.png" alt="exclusive image" className="offer-image"></img>
+                        </div>
+                        <div className="col-2">
+                            <p>Exclusive Available On Accessories Store</p>
+                            <h1>Smart Band 4</h1>
+                            <small>The Mi Smart Band 4 feature a 39.9% larger (than Mi Band 3) AMOLED color full-touch display with adjustable brightness, so everything is clear as can be.<br /></small>
+                            <a href="/" className="btn">Buy Now &#8594;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="testimonial">
+                <div className="small-container">
+                    <div className="row">
+                        <div className="col-3">                    
+                            <i className="fa fa-quote-left" aria-hidden="true"></i>
+                            <p>Lorem input is simply dummy text of the printing and typesetting industry. Lorem input has been the industry's standard dummy text ever.</p>
+                            <div className="rating">
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star-half" aria-hidden="true"></i>
+                            </div> 
+                            <img src="../images/user-1.png" alt="user 1 image"></img>
+                            <h3>Sean Parker</h3>
+                        </div>
+                        <div className="col-3">                    
+                            <i className="fa fa-quote-left" aria-hidden="true"></i>
+                            <p>Lorem input is simply dummy text of the printing and typesetting industry. Lorem input has been the industry's standard dummy text ever.</p>
+                            <div className="rating">
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star-half" aria-hidden="true"></i>
+                            </div> 
+                            <img src="../images/user-2.png" alt="user 2 image"></img>
+                            <h3>Mike Smith</h3>
+                        </div>
+                        <div className="col-3">                    
+                            <i className="fa fa-quote-left" aria-hidden="true"></i>
+                            <p>Lorem input is simply dummy text of the printing and typesetting industry. Lorem input has been the industry's standard dummy text ever.</p>
+                            <div className="rating">
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star" aria-hidden="true"></i>
+                                <i className="fa fa-star-half" aria-hidden="true"></i>
+                            </div> 
+                            <img src="../images/user-3.png" alt="user 3 image"></img>
+                            <h3>Mabel Joe</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="brand">
+                <div className="small-container">
+                    <div className="row">
+                        <div className="col-5">
+                            <img src="../images/logo-coca-cola.png" alt=""></img>
+                        </div>
+                        <div className="col-5">
+                            <img src="../images/logo-godrej.png" alt=""></img>
+                        </div>
+                        <div className="col-5">
+                            <img src="../images/logo-oppo.png" alt=""></img>
+                        </div>
+                        <div className="col-5">
+                            <img src="../images/logo-paypal.png" alt=""></img>
+                        </div>
+                        <div className="col-5">
+                            <img src="../images/logo-philips.png" alt=""></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="footer-col-1">
+                            <img src="../images/logo-white.png" alt="logo footer"></img>
+                            <p>Our purpose is to sustainably Make the Pleasure and Benefits of Sports Accessible to the Many</p>
+                        </div>
+                        <div className="footer-col-2">
+                            <h3>Useful Links</h3>
+                            <ul>
+                                <li>Coupons</li>
+                                <li>Return Policy</li>
+                                <li>Join Affiliate</li>
+                            </ul>
+                        </div>
+                        <div className="footer-col-3">
+                            <h3>Follow Us</h3>
+                            <ul>
+                                <li>Facebook</li>
+                                <li>Instagram</li>
+                                <li>Youtube</li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="create">
+                        <p>Design By Trương Dương Khang - MSSV 1811546141</p>
+                    </div>
+                </div>
             </div>
         </div>
-
     )
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,50 +1,64 @@
 import React from 'react'
+import Rating from '../components/Rating';
+import {Link} from 'react-router-dom';
+import data from '../data'
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    // lấy sản phẩm mà id truyền từ props trùng với id ở data
+    const product = data.products.find((x) => x._id === props.match.params.id);
+
+    // nếu không có sản phẩm, trả thông báo null
+    if(!product) {
+        return <div>Product Not Found !!! </div>
+    }
+
     return (
         <div>
             <div class="container">
                 <div class="navbar">
                     <div class="logo">
-                        <a href="index.html"><img src="images/logo.png" alt="Logo Store" width="125px"></img></a>
+                        <a href="/"><img src="../images/logo.png" alt="Logo Store" width="125px"></img></a>
                     </div>
                     <nav>
                         <ul id = "MenuItems">
-                            <li><a href="index.html">Home</a></li>
+                            <li><a href="/">Home</a></li>
                             <li><a href="product.html">Product</a></li>
                             <li><a href="">About</a></li>
                             <li><a href="">Contact</a></li>
                             <li><a href="account.html">Account</a></li>
                         </ul>
                     </nav>
-                    <a href="cart.html"><img src="images/cart.png" alt="icon cart" class="cart-icon"></img></a>
-                    <img src="images/menu.png" alt="icon menu" class="menu-icon" onclick="menutoggle()"></img>
+                    <a href="cart.html"><img src="../images/cart.png" alt="icon cart" class="cart-icon"></img></a>
+                    <img src="../images/menu.png" alt="icon menu" class="menu-icon" onclick="menutoggle()"></img>
                 </div>
             </div>    
 
-            <div class="small-container single-product">
+            <div class="small-container single-product">                
+                <Link to="/">Back to home</Link>
                 <div class="row">
                     <div class="col-2">
-                        <img src="images/gallery-1.jpg" alt="product detail image" width="100%" id="ProducImg"></img>
+                        <img src={product.image} alt ={product.name}></img>
                         <div class="small-img-row">
                             <div class="small-img-col">
-                                <img src="images/gallery-1.jpg" alt="mini image product" width="100%" class="small-img"></img>
+                                <img src="../images/gallery-1.jpg" alt="mini image product" width="100%" class="small-img"></img>
                             </div>
                             <div class="small-img-col">
-                                <img src="images/gallery-2.jpg" alt="mini image product" width="100%" class="small-img"></img>
+                                <img src="../images/gallery-2.jpg" alt="mini image product" width="100%" class="small-img"></img>
                             </div>
                             <div class="small-img-col">
-                                <img src="images/gallery-3.jpg" alt="mini image product" width="100%" class="small-img"></img>
+                                <img src="../images/gallery-3.jpg" alt="mini image product" width="100%" class="small-img"></img>
                             </div>
                             <div class="small-img-col">
-                                <img src="images/gallery-4.jpg" alt="mini image product" width="100%" class="small-img"></img>
+                                <img src="../images/gallery-4.jpg" alt="mini image product" width="100%" class="small-img"></img>
                             </div>
                         </div>
                     </div>
                     <div class="col-2">
-                        <p>Home / T - Shirt</p>
-                        <h1>Red Printd T-Shirt By HXR</h1>
-                        <h4>100000 VNĐ</h4>
+                        <p>Home / {product.category}</p>
+                        <h2>{product.name}</h2>
+                        <br/>
+                        <Rating rating ={product.rating} numReviews = {product.numReviews}></Rating>
+                        <h4>{product.price}</h4>
                         <select>
                             <option>Select Size</option>
                             <option>XXL</option>
@@ -53,6 +67,10 @@ export default function ProductScreen() {
                             <option>Medium</option>
                             <option>Small</option>
                         </select>
+                        <div>
+                            <h4>Status </h4>
+                            <div>{product.countInStock > 0 ? (<span className="success"> In Stock </span>):(<span className="error"> Unavailable </span>) }</div>
+                        </div>
                         <input type="number" value="1"></input>
                         <a href="" class="btn">Add To Cart</a>
                         <h3>
@@ -60,7 +78,7 @@ export default function ProductScreen() {
                             <i class="fa fa-indent" aria-hidden="true"> </i>
                         </h3>
                         <br/>
-                        <p>Give your summer wardrobe a style upgrade with the HXR Men's Active T-Shirt. Team it with a pair of shorts for your morning workout or a dením for an evening out with the guys</p>
+                        <p>{product.description}</p>
                     </div>
                 </div>
             </div>
@@ -77,7 +95,7 @@ export default function ProductScreen() {
 
                     <div class="row">
                         <div class="col-4">
-                            <img src="images/product-9.jpg" alt="product 9 image"></img>
+                            <img src="../images/product-9.jpg" alt="product 9 image"></img>
                             <h4>Red Printed T-Shirt</h4>
                             <div class="rating">
                                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -89,7 +107,7 @@ export default function ProductScreen() {
                             <p>100000VNĐ</p>
                         </div>
                         <div class="col-4">
-                            <img src="images/product-10.jpg" alt="product 10 image"></img>
+                            <img src="../images/product-10.jpg" alt="product 10 image"></img>
                             <h4>Red Printed T-Shirt</h4>
                             <div class="rating">
                                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -101,7 +119,7 @@ export default function ProductScreen() {
                             <p>100000VNĐ</p>
                         </div>
                         <div class="col-4">
-                            <img src="images/product-11.jpg" alt="product 11 image"></img>
+                            <img src="../images/product-11.jpg" alt="product 11 image"></img>
                             <h4>Red Printed T-Shirt</h4>
                             <div class="rating">
                                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -113,7 +131,7 @@ export default function ProductScreen() {
                             <p>100000VNĐ</p>
                         </div>                
                         <div class="col-4">
-                            <img src="images/product-12.jpg" alt="product 12 image"></img>
+                            <img src="../images/product-12.jpg" alt="product 12 image"></img>
                             <h4>Red Printed T-Shirt</h4>
                             <div class="rating">
                                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -132,7 +150,7 @@ export default function ProductScreen() {
                 <div class="container">
                     <div class="row">
                         <div class="footer-col-1">
-                            <img src="images/logo-white.png" alt="logo footer"></img>
+                            <img src="../images/logo-white.png" alt="logo footer"></img>
                             <p>Our purpose is to sustainably Make the Pleasure and Benefits of Sports Accessible to the Many</p>
                         </div>
                         <div class="footer-col-2">
