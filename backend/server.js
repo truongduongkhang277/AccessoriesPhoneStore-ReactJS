b334a4fd0ea704a6ef9187eab8ca2eb3835d4be9
Truong Duong Khang <truongduongkhang277@gmail.com>
2021-08-14 23:33:23 +0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,19 @@
 import express from 'express';
 import data from './data.js';
 
-var express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-     extended: true
-}));
 
- // route mặc định
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-// lấy api product từ file data.js 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
+})
+
+const port = process.env.PORT || 5000; 
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
 });
 
- // chỉnh port
- app.listen(5000, function () {
-    console.log('Server at http://localhost:5000');
- });
- 
- module.exports = app;
